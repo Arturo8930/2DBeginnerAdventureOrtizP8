029ddb15b9d43f7ae5643df4d0366551dabb5e7d
Arturo Ortiz <753788@student.dusd.net>
2024-10-22 15:30:14 -0700

    Added Movement
    I created movement for my character on unity.

--- 2DBeginnerAdventureP8Ortiz/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP8Ortiz/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,14 +8,19 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
+
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
         Vector2 position = transform.position;
-        position.x=position.x + 0.1f;
+        position.x = position.x + 0.01f * horizontal;
+        position.y = position.y + 0.01f * vertical;
         transform.position = position;
     }
 }
