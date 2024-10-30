131283b5d19dd7ee936c0ca360f19587666809f6
Arturo Ortiz <753788@student.dusd.net>
2024-10-30 15:25:19 -0700

    Collision
    Added collision to Player on unity.

--- 2DBeginnerAdventureP8Ortiz/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP8Ortiz/Assets/Scripts/PlayerController.cs
@@ -2,13 +2,21 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    //Variables related to player character movement
+    public InputAction MoveAction;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-      
+        MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
 
